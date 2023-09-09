e3a800e6849c72df28a3780174f1692abc3300e8
ramphy0x90 <ramphy11@hotmail.com>
2023-09-09 19:24:12 +0200

    Update

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { BooksComponent } from "./containers/books/books.component";
+import { AuthorsComponent } from "./containers/authors/authors.component";
+import { LogComponent } from "./containers/log/log.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+	{ path: "", component: BooksComponent },
+	{ path: "books", pathMatch: "full", redirectTo: "" },
+	{ path: "authors", component: AuthorsComponent },
+	{ path: "logs", component: LogComponent },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+	imports: [RouterModule.forRoot(routes, { useHash: true })],
+	exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
