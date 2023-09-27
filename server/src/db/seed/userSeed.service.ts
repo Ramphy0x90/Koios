fd1d3d22a67e2ab7ae5bdfd14324184ae6fbada3
ramphy0x90 <ramphy11@hotmail.com>
2023-09-27 23:08:28 +0200

    Update

--- server/src/db/seed/userSeed.service.ts
+++ server/src/db/seed/userSeed.service.ts
@@ -3,6 +3,9 @@ import { InjectModel } from "@nestjs/mongoose";
 import { Model } from "mongoose";
 import { AuthService } from "src/auth/services/auth.service";
 import { User } from "src/schemas/user.schema";
+import * as path from "path";
+import { Row } from "exceljs";
+const ExcelJS = require("exceljs");
 
 @Injectable()
 export class UserSeedService {
@@ -11,7 +14,17 @@ export class UserSeedService {
 		private authService: AuthService
 	) {}
 
+	async getDataFromFile(): Promise<void> {
+		// const filePath = path.resolve(__dirname, "../DoppioniCDE.xlsx");
+		// const workbook = new ExcelJS.Workbook();
+		// const content = await workbook.xlsx.readFile(filePath);
+		// const worksheet = content.worksheets[1];
+		// const rows: Row[] = worksheet.getRows(1, 7) ?? [];
+		// console.log(rows[0].getCell[0]);
+	}
+
 	async seedData() {
+		this.getDataFromFile();
 		const password = await this.authService.hashPassword("password");
 
 		const defaultData = [
